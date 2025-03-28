bcd4378be98fc8a94ebdeb189c1d5131879ea959
tamsir <tamsirababacardiouf@gmail.com>
2025-03-28 03:26:38 +0000

    new

---
+++ database.js
@@ -0,0 +1,20 @@
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+
+     host: 'localhost',   
+    user: 'root',
+    password: 'Tam@1#',
+    database: 'api2ie',
+
+
+    
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
+  
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var mysql   = require("mysql");
+var cors = require('cors');
+var bodyParser  = require("body-parser");
+
+
+
+var verifyToken = require('./middleware/verifyToken');
+var addNewUser = require('./middleware/addNewUser');
+var userLoginCheck = require('./middleware/userLoginCheck');
+
+
+var welcome = require('./middleware/welcome');
+ var Utilisateur = require('./middleware/Data/Utilisateur');
+
+
+const port = process.env.PORT || 4400;   
+
+  
+////  Routes  principales  
+
+var app  = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', welcome);
+app.post('/signup', addNewUser);
+app.post('/userlogin', userLoginCheck);
+
+
+////  Sous-Routes avec Token
+
+var apiRoutes = express.Router();
+apiRoutes.use(bodyParser.urlencoded({ extended: true }));
+apiRoutes.use(bodyParser.json());
+
+apiRoutes.use(verifyToken);
+apiRoutes.get('/Utilisateur', Utilisateur);
+
+
+app.use('/api', apiRoutes);
+
+
+app.listen( port , () => {
+    console.log('Démarrage et écoute sur le port  ' +port);
+});
